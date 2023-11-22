cb01498b162a51161f8742e348a0b2bda56594dc
Matheus Werneck <matheus.werneck@chase.io>
2023-11-22 20:18:32 -0300

    refactor: changed browser router to hash router

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,15 +1,71 @@
-import { HashRouter as Router, Route, Routes  } from "react-router-dom";
-import Bfs from "./BreadthFirstSearch";
-import Dfs from './DepthFirstSearch';
+import { useEffect, useState } from "react";
+import "./App.css";
+import bfsData from "./resultBFS.json";
+import dfsData from "./resultDFS.json";
+import Chessboard from "./Chessboard";
+
+const dataByAlgorithm = {
+  bfs: bfsData,
+  dfs: dfsData,
+};
 
 const App = () => {
+  const [index, setIndex] = useState(0);
+  const [algorithm, setAlgorithm] = useState("bfs");
+  const [currentBoardState, setCurrentBoardState] = useState();
+
+  const createBasicState = () => {
+    const board = [];
+
+    const currentState = dataByAlgorithm[algorithm][`${index}`];
+
+    for (let i = 0; i < currentState.length; i++) {
+      board.push(currentState[i]);
+    }
+
+    return board;
+  };
+
+  const resetState = () => {
+    const board = [];
+
+    const currentState = dataByAlgorithm[algorithm][0];
+
+    for (let i = 0; i < currentState.length; i++) {
+      board.push(currentState[i]);
+    }
+
+    setIndex(1);
+    setCurrentBoardState(board);
+  };
+
+  const handleNewBoardState = () => {
+    setIndex((prevState) => prevState + 1);
+    setCurrentBoardState(createBasicState());
+  };
+
+  const changeAlgorithm = () => {
+    if (algorithm === "bfs") setAlgorithm("dfs");
+    else setAlgorithm("bfs");
+  };
+
+  useEffect(() => {
+    setCurrentBoardState(createBasicState());
+  }, []);
+
+  useEffect(() => {
+    resetState();
+  }, [algorithm]);
+
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" Component={Bfs} />
-        <Route exact path="/dfs" Component={Dfs} />
-      </Routes>
-    </Router>
+    <div>
+      <h1>
+        {algorithm === "bfs" ? "Busca em Largura " : "Busca em Profundidade"}
+      </h1>
+      <Chessboard boardState={currentBoardState} />
+      <button onClick={handleNewBoardState}>Proximo Estado</button>
+      <button onClick={changeAlgorithm}> Mudar algoritmo </button>
+    </div>
   );
 };
 
